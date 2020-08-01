ee70703681e47e6d931757f29a1571d25fb29b75
rksharma <rksharma2180@gmail.com>
2020-08-01 22:28:44 +0530

    'Basics'

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;//readonly struct System.Int32
+            double d = 3.2;//System.Double
+            string s = "ravi"; //System.String coding standard is use small string
+            string s1 = s.ToUpper();
+            string s2 = s1.ToLower();
+
+            Console.WriteLine(number);
+            Console.WriteLine(d);
+            Console.WriteLine(s);
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;//library
+
+namespace HelloWorld //project name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Console.ReadLine());
+            Console.ReadKey();
+        }
+    }
+}
